8300229f7a1463637aecb1fbd7448b01ec649c15
张盼 <pan.zhang@yi23.net>
2019-05-23 18:17:20 +0800

    集成flyway

---
+++ mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/MerchantsApplication.java
@@ -0,0 +1,11 @@
+package com.sxzhongf.mscx.passbook;
+
+/**
+ * MerchantsApplication for TODO
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
+ * @see
+ * @since 2019/5/23
+ */
+public class MerchantsApplication {
+}
