7d79d7f68dbc0beb3ad5af841500d02865da93e3
张盼 <pan.zhang@yi23.net>
2019-05-23 14:05:27 +0800

    passbook service

---
+++ src/main/java/com/sxzhongf/mscx/passbook/MscxPassbookApplication.java
@@ -0,0 +1,13 @@
+package com.sxzhongf.mscx.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscxPassbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscxPassbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxzhongf/mscx/passbook/MscxPassbookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sxzhongf.mscx.passbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MscxPassbookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
