bcb2f670e7bc56dd530da5ed3ef9e82358cdf5f2
张盼 <pan.zhang@yi23.net>
2019-05-24 20:49:46 +0800

    注册商户拦截器 & web请求路径

--- mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/MerchantsApplication.java
+++ mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/MerchantsApplication.java
@@ -1,7 +1,11 @@
 package com.sxzhongf.mscx.passbook;
 
+import com.sxzhongf.mscx.passbook.security.AuthCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 卡券系统商户端启动类
@@ -11,8 +15,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2019/5/23
  */
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private AuthCheckInterceptor authCheckInterceptor;
+
     public static void main(String[] args) {
         SpringApplication.run(MerchantsApplication.class, args);
+    }
+
+    /**
+     * 注册自定义拦截器
+     * @param registry {@link InterceptorRegistry}
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authCheckInterceptor)
+                .addPathPatterns("/merchants/**");
+        super.addInterceptors(registry);
     }
 }
---
+++ mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/controller/MerchantsController.java
@@ -0,0 +1,52 @@
+package com.sxzhongf.mscx.passbook.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.sxzhongf.mscx.passbook.service.IMerchantsService;
+import com.sxzhongf.mscx.passbook.vo.CommonResponseVO;
+import com.sxzhongf.mscx.passbook.vo.CreateMerchantsRequestVO;
+import com.sxzhongf.mscx.passbook.vo.PassTemplateVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * MerchantsController for 商户服务web提供
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
+ * @see
+ * @since 2019/5/24
+ */
+@Slf4j
+@RestController
+@RequestMapping(path = "/merchants")
+public class MerchantsController {
+
+    /**
+     * 商户服务接口注入
+     */
+    private final IMerchantsService merchantsService;
+
+    @Autowired
+    public MerchantsController(IMerchantsService merchantsService) {
+        this.merchantsService = merchantsService;
+    }
+
+    @PostMapping("/create")
+    public CommonResponseVO createMerchants(@RequestBody CreateMerchantsRequestVO requestVO) {
+        log.info("创建商户信息: {}", JSON.toJSONString(requestVO));
+        return merchantsService.createMerchants(requestVO);
+    }
+
+    @GetMapping("/{id}")
+    public CommonResponseVO getMerchants(@PathVariable Integer id) {
+        CommonResponseVO responseVO = merchantsService.buildMerchantsInfoById(id);
+        log.info("获取商户{} 信息: {}", id, JSON.toJSONString(responseVO));
+        return responseVO;
+    }
+
+    @PostMapping("/launch")
+    public CommonResponseVO createMerchants(@RequestBody PassTemplateVO passTemplateVO) {
+        log.info("商户发送优惠券信息: {}", JSON.toJSONString(passTemplateVO));
+        return merchantsService.launchPassTemplate(passTemplateVO);
+    }
+}
