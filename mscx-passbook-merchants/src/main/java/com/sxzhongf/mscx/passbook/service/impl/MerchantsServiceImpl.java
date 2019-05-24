08a26b9a074c19f85415f7829f301af8c2940042
张盼 <pan.zhang@yi23.net>
2019-05-24 20:36:29 +0800

    发送消息到kafka

--- mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/service/impl/MerchantsServiceImpl.java
+++ mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/service/impl/MerchantsServiceImpl.java
@@ -1,6 +1,9 @@
 package com.sxzhongf.mscx.passbook.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.sxzhongf.mscx.passbook.constant.Constants;
 import com.sxzhongf.mscx.passbook.dao.MerchantsDao;
+import com.sxzhongf.mscx.passbook.entity.Merchants;
 import com.sxzhongf.mscx.passbook.enums.ErrorCodeEnum;
 import com.sxzhongf.mscx.passbook.service.IMerchantsService;
 import com.sxzhongf.mscx.passbook.vo.CommonResponseVO;
@@ -9,6 +12,7 @@ import com.sxzhongf.mscx.passbook.vo.CreateMerchantsResponseVO;
 import com.sxzhongf.mscx.passbook.vo.PassTemplateVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,11 +28,20 @@ import javax.transaction.Transactional;
 @Service
 public class MerchantsServiceImpl implements IMerchantsService {
 
+    /**
+     * Merchants 数据库接口
+     */
     private final MerchantsDao merchantsDao;
 
+    /**
+     * kafka 客户端
+     */
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Autowired
-    public MerchantsServiceImpl(MerchantsDao merchantsDao) {
+    public MerchantsServiceImpl(MerchantsDao merchantsDao, KafkaTemplate<String, String> kafkaTemplate) {
         this.merchantsDao = merchantsDao;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
@@ -52,11 +65,34 @@ public class MerchantsServiceImpl implements IMerchantsService {
 
     @Override
     public CommonResponseVO buildMerchantsInfoById(Integer id) {
-        return null;
+        CommonResponseVO responseVO = new CommonResponseVO();
+        Merchants merchants = merchantsDao.getById(id);
+        if (null == merchants) {
+            responseVO.setErrorCode(ErrorCodeEnum.MERCHANT_NOT_EXIST.getCode());
+            responseVO.setErrorMsg(ErrorCodeEnum.MERCHANT_NOT_EXIST.getDesc());
+        }
+        responseVO.setData(merchants);
+        return responseVO;
     }
 
     @Override
     public CommonResponseVO launchPassTemplate(PassTemplateVO passTemplateVO) {
-        return null;
+
+        CommonResponseVO responseVO = new CommonResponseVO();
+        ErrorCodeEnum errorCode = passTemplateVO.validate(merchantsDao);
+        if (errorCode != ErrorCodeEnum.SUCCESS) {
+            responseVO.setErrorCode(errorCode.getCode());
+            responseVO.setErrorMsg(errorCode.getDesc());
+        } else {
+            String passTemplate = JSON.toJSONString(passTemplateVO);
+            kafkaTemplate.send(
+                    Constants.TEMPLATE_TOPIC,
+                    Constants.TEMPLATE_TOPIC,
+                    passTemplate
+            );
+            log.info("launchPassTemplate :{}", passTemplate);
+        }
+
+        return responseVO;
     }
 }
--- mscx-passbook-merchants/src/test/java/com/sxzhongf/mscx/passbook/service/MerchantsServiceTest.java
+++ mscx-passbook-merchants/src/test/java/com/sxzhongf/mscx/passbook/service/MerchantsServiceTest.java
@@ -2,7 +2,9 @@ package com.sxzhongf.mscx.passbook.service;
 
 import com.alibaba.fastjson.JSON;
 import com.sxzhongf.mscx.passbook.vo.CreateMerchantsRequestVO;
+import com.sxzhongf.mscx.passbook.vo.PassTemplateVO;
 import net.bytebuddy.utility.RandomString;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 
 /**
  * MerchantsServiceTest for 商户服务测试类
@@ -25,8 +28,11 @@ public class MerchantsServiceTest {
     @Autowired
     private IMerchantsService merchantsService;
 
-    /** 添加上 {@link Transactional},系统会探测到是测试方法
-     * ，添加数据成功后，会自动回滚事务*/
+    /**
+     * 添加上 {@link Transactional},系统会探测到是测试方法
+     * ，添加数据成功后，会自动回滚事务
+     * {"data":{"id":19},"errorCode":0,"errorMsg":""} 单数数据库没有该19的数据
+     */
     @Test
     @Transactional
     public void testCreateMerchantsSuccess() {
@@ -41,6 +47,9 @@ public class MerchantsServiceTest {
         System.out.println(JSON.toJSONString(merchantsService.createMerchants(requestVO)));
     }
 
+    /**
+     * {"data":{"id":-1},"errorCode":1,"errorMsg":"商户名称重复"}
+     */
     @Test
     public void testCreateMerchantsFail() {
         CreateMerchantsRequestVO requestVO = new CreateMerchantsRequestVO();
@@ -53,4 +62,60 @@ public class MerchantsServiceTest {
 
         System.out.println(JSON.toJSONString(merchantsService.createMerchants(requestVO)));
     }
+
+    /**
+     * 查找 名称为《众福》的商户
+     * {"data":
+     * {
+     * "address":"陕西渭南蒲城县",
+     * "businessLicenseUrl":"www.sxzhongf.com",
+     * "id":18,
+     * "isAudit":false,
+     * "logoUrl":"www.sxzhongf.com",
+     * "name":"众福kU",
+     * "phone":"0913-7196961"
+     * },
+     * "errorCode":0,
+     * "errorMsg":""
+     * }
+     */
+    @Test
+    public void testBuildMerchantsInfoByIdSuccess() {
+        System.out.println(JSON.toJSONString(merchantsService.buildMerchantsInfoById(18)));
+    }
+
+    /**
+     * 查找一个不存在的商户
+     * {"errorCode":6,"errorMsg":"商户不存在"}
+     */
+    @Test
+    public void testBuildMerchantsInfoByIdFail() {
+        System.out.println(JSON.toJSONString(merchantsService.buildMerchantsInfoById(1)));
+    }
+
+    /**
+     * 消息投放到kafka测试
+     * launchPassTemplate :{"background":2,"desc":"详情: 众福优惠券"
+     * ,"end":1559565153495,"hasToken":false,"id":18
+     * ,"limit":10000,"start":1558701153495,"summary":"简介: 众福优惠券"
+     * ,"title":"title: 众福"}
+     */
+    @Test
+    public void testLaunchPassTemplate() {
+        PassTemplateVO passTemplateVO = new PassTemplateVO();
+        //设置商户id为18
+        passTemplateVO.setId(18);
+        passTemplateVO.setTitle("title: 众福");
+        passTemplateVO.setSummary("简介: 众福优惠券");
+        passTemplateVO.setDesc("详情: 众福优惠券");
+        passTemplateVO.setLimit(10000L);
+        passTemplateVO.setHasToken(false);
+        passTemplateVO.setBackground(2);
+        passTemplateVO.setStart(new Date());
+        passTemplateVO.setEnd(DateUtils.addDays(new Date(), 10));
+
+        System.out.println(JSON.toJSONString(merchantsService.launchPassTemplate(passTemplateVO)));
+
+    }
+
 }
