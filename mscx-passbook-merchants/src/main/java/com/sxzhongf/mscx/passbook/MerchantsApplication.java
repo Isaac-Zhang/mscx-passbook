b4cb1473ebb259fddca656979449614ee448b459
张盼 <pan.zhang@yi23.net>
2019-05-24 08:14:17 +0800

    prepare the project env.

--- mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/MerchantsApplication.java
+++ mscx-passbook-merchants/src/main/java/com/sxzhongf/mscx/passbook/MerchantsApplication.java
@@ -1,11 +1,18 @@
 package com.sxzhongf.mscx.passbook;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * MerchantsApplication for TODO
+ * 卡券系统商户端启动类
  *
  * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
  * @see
  * @since 2019/5/23
  */
+@SpringBootApplication
 public class MerchantsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantsApplication.class, args);
+    }
 }
