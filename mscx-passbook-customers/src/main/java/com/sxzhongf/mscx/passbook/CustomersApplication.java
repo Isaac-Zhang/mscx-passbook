6414659168d6405770f36293f65042f09cde8172
IsaacZhang <zhangpan0614@126.com>
2019-05-29 15:44:54 +0800

    更新配置文件信息，创建hbase表脚本

---
+++ mscx-passbook-customers/src/main/java/com/sxzhongf/mscx/passbook/CustomersApplication.java
@@ -0,0 +1,19 @@
+package com.sxzhongf.mscx.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CustomersApplication for 卡券系统客户端启动类
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang</a>
+ * @see
+ * @since 2019/5/29
+ */
+@SpringBootApplication
+public class CustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
+}
